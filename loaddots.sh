bd4ea6f91a6506899cfb8dad0d73f5d829d04386
Jeff Lee <jlee@fool.com>
2014-12-25 23:03:18 -0500

    make safe load and store scripts

--- loaddots.sh
+++ loaddots.sh
@@ -7,9 +7,37 @@ load () {
     if [ -f ~/.$FILE ]; then
         echo "Backing up $FILE< overwriting>"
         cp ~/.$FILE ~/.$FILE.bak.$(date +"%Y-%m-%d.%H-%M-%S")
+    else
+        echo "Creating new file $FILE"
     fi
     cp $DIR/$FILE ~/.$FILE
 }
 
-load "tmux.conf"
-load "vimrc"
+doStuff() {
+    load "tmux.conf"
+    load "vimrc"
+
+    cd ~/.vim/bundle
+    for i in $(cat $DIR/plugins.txt); do
+        git clone $i 2> /dev/null
+        code=$?
+
+        if [[ code -eq 0 ]]; then
+            echo "Cloned $i"
+        elif [[ code -eq 128 ]]; then
+            echo "Skipping $i (already loaded)"
+        else
+            echo "Unknown error: $code"
+        fi
+    done
+}
+
+read -p "Applying this git repo's dotfiles to your current system. All files will be backed up in home directory. Do you want to continue? (y/n)" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo
+    doStuff
+else
+    echo
+    exit 1
+fi
--- storedots.sh
+++ storedots.sh
@@ -4,10 +4,42 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 store () {
     FILE=$1
-    echo "$FILE changing from< to>"
-    diff $DIR/$FILE ~/.$FILE
-    cp ~/.$FILE $DIR/$FILE
+
+    echo "Diffing $DIR/$FILE and ~/.$FILE"
+    diff $DIR/$FILE ~/.$FILE > /dev/null
+    code=$?
+    if [[ $code -eq 1 ]]
+    then
+        echo "$FILE changing from< to>"
+        diff $DIR/$FILE ~/.$FILE
+        cp ~/.$FILE $DIR/$FILE
+    elif [[ $code -eq 0 ]]
+    then
+        echo "$FILE has no changes"
+    else
+        echo "Unknown diff exit code $code"
+    fi
 }
 
-store "tmux.conf"
-store "vimrc"
+doStuff() {
+    echo -n > plugins.txt
+    echo -n "Logging plugins... "
+    for i in $(ls ~/.vim/bundle); do
+        echo -n "$i "
+        cat ~/.vim/bundle/$i/.git/config | sed -n "s/\s*url = \(.\)/\1/p" >> plugins.txt
+    done
+    echo
+
+    store "tmux.conf"
+    store "vimrc"
+}
+
+read -p "Backing up current system configurations into this git repo. Do you want to continue? (y/n)" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo
+    doStuff
+else
+    echo
+    exit 1
+fi
